fad767119728436447613188153ec8eac4d7f0cc
trungle <leductrungxf@gmail.com>
2022-01-15 13:51:24 +0100

    Add info widget

--- widgets.py
+++ widgets.py
@@ -10,8 +10,8 @@ def new_factory(news: List[Dict]) -> v.Html:
             text=True,
             block=True,
             children=["Open"],
-            href=new['link'],
-            target="_blank"
+            href=new["link"],
+            target="_blank",
         )
         card = v.Card(
             outlined=True,
@@ -23,3 +23,23 @@ def new_factory(news: List[Dict]) -> v.Html:
         )
         children.append(card)
     return v.Html(tag="div", class_="d-flex flex-column", children=children)
+
+
+def financial_info_factory(data: List[Dict]) -> v.Html:
+    children = []
+    for item in data:
+        card = v.Card(
+            outlined=True,
+            class_="ma-1",
+            children=[
+                v.CardTitle(
+                    primary_title=True,
+                    children=[item["title"]],
+                    style_="font-size: 1.1rem",
+                ),
+                v.CardText(children=[str(item["value"])]),
+            ],
+            style_="width: calc(16.667% - 8px)",
+        )
+        children.append(card)
+    return v.Html(tag="div", class_="d-flex flex-row", children=children)
