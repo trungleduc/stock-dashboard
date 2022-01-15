2e317eda3348884aab2f120be0f1a2d3675ddc31
trungle <leductrungxf@gmail.com>
2022-01-15 23:19:33 +0100

    Add price widget

--- ticker.py
+++ ticker.py
@@ -1,23 +1,27 @@
 from typing import Dict, List
 import yfinance as yf
 
-class TickerData:
-  def __init__(self, ticker: str) -> None:
-      self._ticker = yf.Ticker(ticker)
-      self._info = self._ticker.get_info()
-      
-  @property
-  def news(self)-> List:
-    return self._ticker.news
 
-  @property
-  def financial_info(self)-> List[Dict]:
-    data = [
-      {"title": "Market cap", "value": self._info['marketCap']},
-      {"title": "PE Ratio", "value": self._info['forwardPE']},
-      {"title": "Total revenue", "value": self._info['totalRevenue']},
-      {"title": "Gross profit", "value": self._info['grossProfits']},
-      {"title": "Debt to equity", "value": self._info['debtToEquity']},
-      {"title": "Profit margin", "value": self._info['profitMargins']},
-    ]
-    return data
+class TickerData:
+    def __init__(self, ticker: str) -> None:
+        self._ticker = yf.Ticker(ticker)
+        self._info = self._ticker.get_info()
+
+    @property
+    def news(self) -> List:
+        return self._ticker.news
+
+    @property
+    def financial_info(self) -> List[Dict]:
+        data = [
+            {"title": "Market cap", "value": self._info["marketCap"]},
+            {"title": "PE Ratio", "value": self._info["forwardPE"]},
+            {"title": "Total revenue", "value": self._info["totalRevenue"]},
+            {"title": "Gross profit", "value": self._info["grossProfits"]},
+            {"title": "Debt to equity", "value": self._info["debtToEquity"]},
+            {"title": "Profit margin", "value": self._info["profitMargins"]},
+        ]
+        return data
+
+    def price(self, history="6mo") -> List:
+        return self._ticker.history(history)
--- widgets.py
+++ widgets.py
@@ -1,5 +1,10 @@
+from turtle import title
 import ipyvuetify as v
 from typing import Dict, List
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd
+import ipywidgets as ipw
 
 
 def new_factory(news: List[Dict]) -> v.Html:
@@ -25,7 +30,7 @@ def new_factory(news: List[Dict]) -> v.Html:
     return v.Html(tag="div", class_="d-flex flex-column", children=children)
 
 
-def financial_info_factory(data: List[Dict]) -> v.Html:
+def financial_info_factory(data: List[Dict], ticker: str = "") -> v.Html:
     children = []
     for item in data:
         card = v.Card(
@@ -39,7 +44,50 @@ def financial_info_factory(data: List[Dict]) -> v.Html:
                 ),
                 v.CardText(children=[str(item["value"])]),
             ],
-            style_="width: calc(16.667% - 8px)",
+            style_="width: calc(16.6% - 8px); min-width: 150px",
         )
         children.append(card)
-    return v.Html(tag="div", class_="d-flex flex-row", children=children)
+    return v.Html(
+        tag="div", class_="d-flex flex-row", children=children, style_="flex-wrap: wrap"
+    )
+
+
+def price_widget_factory(df: List, ticker: str = "") -> ipw.Widget:
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # include candlestick with rangeselector
+    fig.add_trace(
+        go.Candlestick(
+            x=df.index,
+            open=df["Open"],
+            high=df["High"],
+            low=df["Low"],
+            close=df["Close"],
+            name="OHLC",
+        ),
+        secondary_y=True,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=df.index,
+            y=df["Volume"],
+            marker_color="rgba(91, 91, 91, 0.73)",
+            name="Volume",
+        ),
+        secondary_y=False,
+    )
+
+    fig.layout.yaxis2.showgrid = False
+    fig.update_layout(
+        xaxis_rangeslider_visible=False,
+        template="plotly_dark",
+        title={
+            "text": f"{ticker.upper()} PRICE CHART",
+            "xanchor": "center",
+            "yanchor": "top",
+            "x": 0.5,
+        },
+    )
+    widget = go.FigureWidget(fig)
+    return widget
