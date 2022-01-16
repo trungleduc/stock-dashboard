d3ba98bbab73291a070379194eb584764504ede4
trungle <leductrungxf@gmail.com>
2022-01-16 12:37:50 +0100

    Add summary widget

--- widgets.py
+++ widgets.py
@@ -1,11 +1,10 @@
-from distutils.command.config import config
-from turtle import title
-import ipyvuetify as v
+from datetime import datetime
 from typing import Dict, List
+
+import ipyvuetify as v
+import ipywidgets as ipw
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import pandas as pd
-import ipywidgets as ipw
 
 v.theme.dark = True
 
@@ -13,6 +12,7 @@ v.theme.dark = True
 def new_factory(news: List[Dict]) -> v.Html:
     children = []
     for new in news:
+        date = datetime.fromtimestamp(new["providerPublishTime"])
         btn = v.Btn(
             small=True,
             text=True,
@@ -24,8 +24,15 @@ def new_factory(news: List[Dict]) -> v.Html:
         card = v.Card(
             outlined=True,
             children=[
-                v.CardTitle(primary_title=True, children=[new["title"]]),
-                v.CardSubtitle(children=[new["publisher"]]),
+                v.CardTitle(
+                    children=[new["title"]],
+                    style_="font-size: 1.1rem",
+                ),
+                v.CardSubtitle(
+                    children=[
+                        f'{new["publisher"]} ({date.strftime("%m/%d/%Y, %H:%M")})'
+                    ]
+                ),
                 v.CardActions(children=[btn]),
             ],
         )
@@ -33,8 +40,17 @@ def new_factory(news: List[Dict]) -> v.Html:
     return v.Html(tag="div", class_="d-flex flex-column", children=children)
 
 
-def financial_info_factory(data: List[Dict], ticker: str = "") -> v.Html:
+def financial_info_factory(data: List[Dict], logo_url: str = None) -> v.Html:
     children = []
+    if logo_url is not None:
+        logo = v.Card(
+            outlined=True,
+            class_="ma-1",
+            children=[v.Img(src=logo_url, height='100px')],
+            style_="width: calc(14.28% - 8px); min-width: 150px",
+        )
+        children.append(logo)
+        
     for item in data:
         card = v.Card(
             outlined=True,
@@ -43,11 +59,11 @@ def financial_info_factory(data: List[Dict], ticker: str = "") -> v.Html:
                 v.CardTitle(
                     primary_title=True,
                     children=[item["title"]],
-                    style_="font-size: 1.1rem",
+                    style_="font-size: 18px; color: #51ef98",
                 ),
                 v.CardText(children=[str(item["value"])]),
             ],
-            style_="width: calc(16.6% - 8px); min-width: 150px",
+            style_="width: calc(14.28% - 8px); min-width: 150px",
         )
         children.append(card)
     return v.Html(
@@ -93,7 +109,7 @@ def price_chart_factory(df: List, ticker: str = "") -> ipw.Widget:
             "x": 0.5,
         },
     )
-    widget = go.FigureWidget(fig, layout=ipw.Layout(height='100%'))
+    widget = go.FigureWidget(fig, layout=ipw.Layout(height="100%"))
     return widget
 
 
@@ -111,3 +127,16 @@ def price_history_factory(df: List, ticker: str = "") -> ipw.Widget:
         },
     )
     return widget
+
+
+def text_widget(title: str, text: str) -> ipw.Widget:
+    return v.Card(
+        outlined=True,
+        children=[
+            v.CardTitle(
+                children=[title],
+                style_="font-size: 1.1rem",
+            ),
+            v.CardText(children=[text]),
+        ],
+    )
